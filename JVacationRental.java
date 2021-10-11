98d63a1e18b08421adc4fcbaccb87cfef02b1138
Sanaa Elmosbah <80988796+elmossanaa@users.noreply.github.com>
2021-10-11 18:03:35 -0400

    Update JVacationRental.java

--- JVacationRental.java
+++ JVacationRental.java
@@ -1,3 +1,8 @@
+/** 1.	Write an application for Lambert’s Vacation Rentals. Use separate  ButtonGroups to allow a client to select one of three locations, the number of bedrooms,
+and whether meals are included in the rental. Assume that the locations are parkside for $600 per week, poolside for $750 per week, or lakeside for $825 per week.
+Assume that the rentals have one, two, or three bedrooms and that each bedroom over one adds $75 to the base price. Assume that if meals are added, the price is $200
+more per rental. Save the file as JVacationRental.**/
+
 import java.awt.event.ActionEvent; 
 
 import java.awt.event.ActionListener;
